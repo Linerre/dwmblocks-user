5a03db8086a54fd70cff3b17ac7cc7a7e41488c4
Linerre <leon.errelin@outlook.com>
2021-02-22 15:26:28 +0800

    mod: add battery icon to status bar

--- blocks.h
+++ blocks.h
@@ -1,10 +1,10 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
+	{" ", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
 
-	{"", "acpi | awk '{print $3, substr($4,1,3)}' | sed s/Discharging,/Bat/",					300,		0},
-	{"", "date '+%b-%d [%a] %I:%M%p'",					60,		0},
+	{"", "acpi | awk '{print $3, substr($4,1,3)}' | sed 's/Discharging,//;s/Charging,//'",	300,		0},
+	{"", "date '+%b-%d %a %I:%M%p'",					60,		0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
