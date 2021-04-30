13e50aba8df56c189f29b838908565fffdb8c04e
Linerre <leon.errelin@outlook.com>
2021-04-30 20:02:44 +0800

    mod: mv scripts to another place

--- blocks.h
+++ blocks.h
@@ -1,11 +1,11 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"", "~/.local/bin/sb-memory",	30,		0},
-	{"", "~/.local/bin/sb-wifi",	60,		0},
-	{"", "~/.local/bin/sb-battery",	60,	0},
-	{"", "~/.local/bin/sb-volume",	0,	11},
-	{"", "~/.local/bin/sb-clock",	60,		0},
+	{"", "~/.local/scripts/sb-memory",	30,		0},
+	{"", "~/.local/scripts/sb-wifi",	60,		0},
+	{"", "~/.local/scripts/sb-battery",	60,	0},
+	{"", "~/.local/scripts/sb-volume",	0,	11},
+	{"", "~/.local/scripts/sb-clock",	60,		0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
