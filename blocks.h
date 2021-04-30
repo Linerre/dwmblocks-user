427ecd5356cd04d2254b397238bdf174b503dd11
Linerre <leon.errelin@outlook.com>
2021-04-30 20:10:56 +0800

    fix: wrong path

--- blocks.h
+++ blocks.h
@@ -1,11 +1,11 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"", "~/.local/scripts/sb-memory",	30,		0},
-	{"", "~/.local/scripts/sb-wifi",	60,		0},
-	{"", "~/.local/scripts/sb-battery",	60,	0},
-	{"", "~/.local/scripts/sb-volume",	0,	11},
-	{"", "~/.local/scripts/sb-clock",	60,		0},
+	{"", "~/.local/scripts/blocks/sb-memory",	30,		0},
+	{"", "~/.local/scripts/blocks/sb-wifi",	60,		0},
+	{"", "~/.local/scripts/blocks/sb-battery",	60,	0},
+	{"", "~/.local/scripts/blocks/sb-volume",	0,	11},
+	{"", "~/.local/scripts/blocks/sb-clock",	60,		0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
