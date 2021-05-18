de9b3a5789953c4cb6fb59b90ce311fcb367fa20
Linerre <leon.errelin@outlook.com>
2021-05-19 05:43:22 +0800

    mod: adjust blocks for gentoo linux

--- blocks.h
+++ blocks.h
@@ -2,9 +2,9 @@
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 	{"", "~/.local/scripts/blocks/sb-memory",	30,		0},
-	{"", "~/.local/scripts/blocks/sb-wifi",	60,		0},
-	{"", "~/.local/scripts/blocks/sb-battery",	60,	0},
-	{"", "~/.local/scripts/blocks/sb-volume",	0,	11},
+	/*{"", "~/.local/scripts/blocks/sb-wifi",	60,		0},*/
+	/*{"", "~/.local/scripts/blocks/sb-battery",	60,	0},*/
+	/*{"", "~/.local/scripts/blocks/sb-volume",	0,	11},*/
 	{"", "~/.local/scripts/blocks/sb-clock",	60,		0},
 };
 
